0cf8ded62a3ad47f14e385de26bb6c07ccc570c7
Maris Erts <7067057+mariserts@users.noreply.github.com>
2022-03-10 09:35:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 
     # Version number (initial):
     version="0.0.1",
-
+    
     # Application author details:
     author="Maris Erts",
     author_email="maris@plain.ie",
@@ -28,7 +28,7 @@ setup(
     # long_description=open("README.txt").read(),
 
     # Dependent packages (distributions)
-    install_requires=[
+    extras_require=[
         "Django==4.0.1",
     ],
 
